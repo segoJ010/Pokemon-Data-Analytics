052064b8cd22fa9e16e531c8da71c246c63bf8c6
segoJ010 <segoc001@gmail.com>
2016-02-02 14:52:14 -0500

    pokemon.js

---
+++ pokemon.js
@@ -0,0 +1,989 @@
+var pie = new d3pie("pieChart", {
+	"header": {
+		"title": {
+			"text": "Mew",
+			"fontSize": 32,
+			"font": "verdana"
+		},
+		"subtitle": {
+			"text": "Psychic Pokemon",
+			"color": "#999999",
+			"fontSize": 20,
+			"font": "verdana"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 11,
+		"font": "open sans",
+		"location": "bottom-center"
+	},
+	"size": {
+		"canvasHeight": 560,
+		"canvasWidth": 650,
+		"pieInnerRadius": "32%",
+		"pieOuterRadius": "78%"
+	},
+	"data": {
+		"content": [
+			{
+				"label": "Health Points",
+				"value": 100,
+				"color": "#20bd35"
+			},
+			{
+				"label": "Attack Points",
+				"value": 105,
+				"color": "#eb1124"
+			},
+			{
+				"label": "Defense Points",
+				"value": 100,
+				"color": "#383c7e"
+			},
+			{
+				"label": "Special Attack",
+				"value": 100,
+				"color": "#69387e"
+			},
+			{
+				"label": "Special Defense",
+				"value": 100,
+				"color": "#73a417"
+			},
+			{
+				"label": "Speed",
+				"value": 100,
+				"color": "#dcb921"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 10
+		},
+		"inner": {
+			"format": "value"
+		},
+		"mainLabel": {
+			"font": "verdana",
+			"fontSize": 15
+		},
+		"percentage": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"fontSize": 13
+		},
+		"lines": {
+			"enabled": true,
+			"color": "#cccccc"
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	}
+});
+
+var pie = new d3pie("pieChart1", {
+	"header": {
+		"title": {
+			"text": "Mewtwo",
+			"fontSize": 32,
+			"font": "verdana"
+		},
+		"subtitle": {
+			"text": "Psychic (Artifical) Pokemon",
+			"color": "#999999",
+			"fontSize": 20,
+			"font": "verdana"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 11,
+		"font": "open sans",
+		"location": "bottom-center"
+	},
+	"size": {
+		"canvasHeight": 560,
+		"canvasWidth": 650,
+		"pieInnerRadius": "32%",
+		"pieOuterRadius": "78%"
+	},
+	"data": {
+		"content": [
+			{
+				"label": "Health Points",
+				"value": 106,
+				"color": "#20bd35"
+			},
+			{
+				"label": "Attack Points",
+				"value": 110,
+				"color": "#eb1124"
+			},
+			{
+				"label": "Defense Points",
+				"value": 90,
+				"color": "#383c7e"
+			},
+			{
+				"label": "Special Attack",
+				"value": 154,
+				"color": "#69387e"
+			},
+			{
+				"label": "Special Defense",
+				"value": 90,
+				"color": "#73a417"
+			},
+			{
+				"label": "Speed",
+				"value": 130,
+				"color": "#dcb921"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 10
+		},
+		"inner": {
+			"format": "value"
+		},
+		"mainLabel": {
+			"font": "verdana",
+			"fontSize": 15
+		},
+		"percentage": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"fontSize": 13
+		},
+		"lines": {
+			"enabled": true,
+			"color": "#cccccc"
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	}
+});
+
+var pie = new d3pie("pieChart2", {
+	"header": {
+		"title": {
+			"text": "Dragonite",
+			"fontSize": 32,
+			"font": "verdana"
+		},
+		"subtitle": {
+			"text": "Dragon Pokemon",
+			"color": "#999999",
+			"fontSize": 20,
+			"font": "verdana"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 11,
+		"font": "open sans",
+		"location": "bottom-center"
+	},
+	"size": {
+		"canvasHeight": 560,
+		"canvasWidth": 650,
+		"pieInnerRadius": "32%",
+		"pieOuterRadius": "78%"
+	},
+	"data": {
+		"content": [
+			{
+				"label": "Health Points",
+				"value": 100,
+				"color": "#20bd35"
+			},
+			{
+				"label": "Attack Points",
+				"value": 100,
+				"color": "#eb1124"
+			},
+			{
+				"label": "Defense Points",
+				"value": 100,
+				"color": "#383c7e"
+			},
+			{
+				"label": "Special Attack",
+				"value": 100,
+				"color": "#69387e"
+			},
+			{
+				"label": "Special Defense",
+				"value": 100,
+				"color": "#73a417"
+			},
+			{
+				"label": "Speed",
+				"value": 100,
+				"color": "#dcb921"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 10
+		},
+		"inner": {
+			"format": "value"
+		},
+		"mainLabel": {
+			"font": "verdana",
+			"fontSize": 15
+		},
+		"percentage": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"fontSize": 13
+		},
+		"lines": {
+			"enabled": true,
+			"color": "#cccccc"
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	}
+});
+
+var pie = new d3pie("pieChart3", {
+	"header": {
+		"title": {
+			"text": "Articuno",
+			"fontSize": 32,
+			"font": "verdana"
+		},
+		"subtitle": {
+			"text": "Ice Pokemon",
+			"color": "#999999",
+			"fontSize": 20,
+			"font": "verdana"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 11,
+		"font": "open sans",
+		"location": "bottom-center"
+	},
+	"size": {
+		"canvasHeight": 560,
+		"canvasWidth": 650,
+		"pieInnerRadius": "32%",
+		"pieOuterRadius": "78%"
+	},
+	"data": {
+		"content": [
+			{
+				"label": "Health Points",
+				"value": 90,
+				"color": "#20bd35"
+			},
+			{
+				"label": "Attack Points",
+				"value": 85,
+				"color": "#eb1124"
+			},
+			{
+				"label": "Defense Points",
+				"value": 100,
+				"color": "#383c7e"
+			},
+			{
+				"label": "Special Attack",
+				"value": 95,
+				"color": "#69387e"
+			},
+			{
+				"label": "Special Defense",
+				"value": 125,
+				"color": "#73a417"
+			},
+			{
+				"label": "Speed",
+				"value": 85,
+				"color": "#dcb921"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 10
+		},
+		"inner": {
+			"format": "value"
+		},
+		"mainLabel": {
+			"font": "verdana",
+			"fontSize": 15
+		},
+		"percentage": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"fontSize": 13
+		},
+		"lines": {
+			"enabled": true,
+			"color": "#cccccc"
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	}
+});
+
+var pie = new d3pie("pieChart4", {
+	"header": {
+		"title": {
+			"text": "Zapdos",
+			"fontSize": 32,
+			"font": "verdana"
+		},
+		"subtitle": {
+			"text": "Electric Pokemon",
+			"color": "#999999",
+			"fontSize": 20,
+			"font": "verdana"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 11,
+		"font": "open sans",
+		"location": "bottom-center"
+	},
+	"size": {
+		"canvasHeight": 560,
+		"canvasWidth": 650,
+		"pieInnerRadius": "32%",
+		"pieOuterRadius": "78%"
+	},
+	"data": {
+		"content": [
+			{
+				"label": "Health Points",
+				"value": 90,
+				"color": "#20bd35"
+			},
+			{
+				"label": "Attack Points",
+				"value": 90,
+				"color": "#eb1124"
+			},
+			{
+				"label": "Defense Points",
+				"value": 85,
+				"color": "#383c7e"
+			},
+			{
+				"label": "Special Attack",
+				"value": 125,
+				"color": "#69387e"
+			},
+			{
+				"label": "Special Defense",
+				"value": 90,
+				"color": "#73a417"
+			},
+			{
+				"label": "Speed",
+				"value": 100,
+				"color": "#dcb921"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 10
+		},
+		"inner": {
+			"format": "value"
+		},
+		"mainLabel": {
+			"font": "verdana",
+			"fontSize": 15
+		},
+		"percentage": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"fontSize": 13
+		},
+		"lines": {
+			"enabled": true,
+			"color": "#cccccc"
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	}
+});
+
+var pie = new d3pie("pieChart5", {
+	"header": {
+		"title": {
+			"text": "Moltres",
+			"fontSize": 32,
+			"font": "verdana"
+		},
+		"subtitle": {
+			"text": "Electric Pokemon",
+			"color": "#999999",
+			"fontSize": 20,
+			"font": "verdana"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 11,
+		"font": "open sans",
+		"location": "bottom-center"
+	},
+	"size": {
+		"canvasHeight": 560,
+		"canvasWidth": 650,
+		"pieInnerRadius": "32%",
+		"pieOuterRadius": "78%"
+	},
+	"data": {
+		"content": [
+			{
+				"label": "Health Points",
+				"value": 90,
+				"color": "#20bd35"
+			},
+			{
+				"label": "Attack Points",
+				"value": 100,
+				"color": "#eb1124"
+			},
+			{
+				"label": "Defense Points",
+				"value": 90,
+				"color": "#383c7e"
+			},
+			{
+				"label": "Special Attack",
+				"value": 125,
+				"color": "#69387e"
+			},
+			{
+				"label": "Special Defense",
+				"value": 85,
+				"color": "#73a417"
+			},
+			{
+				"label": "Speed",
+				"value": 90,
+				"color": "#dcb921"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 10
+		},
+		"inner": {
+			"format": "value"
+		},
+		"mainLabel": {
+			"font": "verdana",
+			"fontSize": 15
+		},
+		"percentage": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"fontSize": 13
+		},
+		"lines": {
+			"enabled": true,
+			"color": "#cccccc"
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	}
+});
+
+var pie = new d3pie("pieChart6", {
+	"header": {
+		"title": {
+			"text": "Snorlax",
+			"fontSize": 32,
+			"font": "verdana"
+		},
+		"subtitle": {
+			"text": "Normal Pokemon",
+			"color": "#999999",
+			"fontSize": 20,
+			"font": "verdana"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 11,
+		"font": "open sans",
+		"location": "bottom-center"
+	},
+	"size": {
+		"canvasHeight": 560,
+		"canvasWidth": 650,
+		"pieInnerRadius": "32%",
+		"pieOuterRadius": "78%"
+	},
+	"data": {
+		"content": [
+			{
+				"label": "Health Points",
+				"value": 160,
+				"color": "#20bd35"
+			},
+			{
+				"label": "Attack Points",
+				"value": 110,
+				"color": "#eb1124"
+			},
+			{
+				"label": "Defense Points",
+				"value": 65,
+				"color": "#383c7e"
+			},
+			{
+				"label": "Special Attack",
+				"value": 65,
+				"color": "#69387e"
+			},
+			{
+				"label": "Special Defense",
+				"value": 110,
+				"color": "#73a417"
+			},
+			{
+				"label": "Speed",
+				"value": 30,
+				"color": "#dcb921"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 10
+		},
+		"inner": {
+			"format": "value"
+		},
+		"mainLabel": {
+			"font": "verdana",
+			"fontSize": 15
+		},
+		"percentage": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"fontSize": 13
+		},
+		"lines": {
+			"enabled": true,
+			"color": "#cccccc"
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	}
+});
+
+var pie = new d3pie("pieChart7", {
+	"header": {
+		"title": {
+			"text": "Flarean",
+			"fontSize": 32,
+			"font": "verdana"
+		},
+		"subtitle": {
+			"text": "Fire Pokemon",
+			"color": "#999999",
+			"fontSize": 20,
+			"font": "verdana"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 11,
+		"font": "open sans",
+		"location": "bottom-center"
+	},
+	"size": {
+		"canvasHeight": 560,
+		"canvasWidth": 650,
+		"pieInnerRadius": "32%",
+		"pieOuterRadius": "78%"
+	},
+	"data": {
+		"content": [
+			{
+				"label": "Health Points",
+				"value": 65,
+				"color": "#20bd35"
+			},
+			{
+				"label": "Attack Points",
+				"value": 130,
+				"color": "#eb1124"
+			},
+			{
+				"label": "Defense Points",
+				"value": 60,
+				"color": "#383c7e"
+			},
+			{
+				"label": "Special Attack",
+				"value": 95,
+				"color": "#69387e"
+			},
+			{
+				"label": "Special Defense",
+				"value": 110,
+				"color": "#73a417"
+			},
+			{
+				"label": "Speed",
+				"value": 65,
+				"color": "#dcb921"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 10
+		},
+		"inner": {
+			"format": "value"
+		},
+		"mainLabel": {
+			"font": "verdana",
+			"fontSize": 15
+		},
+		"percentage": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"fontSize": 13
+		},
+		"lines": {
+			"enabled": true,
+			"color": "#cccccc"
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	}
+});
+
+var pie = new d3pie("pieChart8", {
+	"header": {
+		"title": {
+			"text": "Jolteon",
+			"fontSize": 32,
+			"font": "verdana"
+		},
+		"subtitle": {
+			"text": "Electric Pokemon",
+			"color": "#999999",
+			"fontSize": 20,
+			"font": "verdana"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 11,
+		"font": "open sans",
+		"location": "bottom-center"
+	},
+	"size": {
+		"canvasHeight": 560,
+		"canvasWidth": 650,
+		"pieInnerRadius": "32%",
+		"pieOuterRadius": "78%"
+	},
+	"data": {
+		"content": [
+			{
+				"label": "Health Points",
+				"value": 65,
+				"color": "#20bd35"
+			},
+			{
+				"label": "Attack Points",
+				"value": 65,
+				"color": "#eb1124"
+			},
+			{
+				"label": "Defense Points",
+				"value": 60,
+				"color": "#383c7e"
+			},
+			{
+				"label": "Special Attack",
+				"value": 110,
+				"color": "#69387e"
+			},
+			{
+				"label": "Special Defense",
+				"value": 95,
+				"color": "#73a417"
+			},
+			{
+				"label": "Speed",
+				"value": 130,
+				"color": "#dcb921"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 10
+		},
+		"inner": {
+			"format": "value"
+		},
+		"mainLabel": {
+			"font": "verdana",
+			"fontSize": 15
+		},
+		"percentage": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"fontSize": 13
+		},
+		"lines": {
+			"enabled": true,
+			"color": "#cccccc"
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	}
+});
+
+var pie = new d3pie("pieChart9", {
+	"header": {
+		"title": {
+			"text": "Vaporean",
+			"fontSize": 32,
+			"font": "verdana"
+		},
+		"subtitle": {
+			"text": "Water Pokemon",
+			"color": "#999999",
+			"fontSize": 20,
+			"font": "verdana"
+		},
+		"titleSubtitlePadding": 9
+	},
+	"footer": {
+		"color": "#999999",
+		"fontSize": 11,
+		"font": "open sans",
+		"location": "bottom-center"
+	},
+	"size": {
+		"canvasHeight": 560,
+		"canvasWidth": 650,
+		"pieInnerRadius": "32%",
+		"pieOuterRadius": "78%"
+	},
+	"data": {
+		"content": [
+			{
+				"label": "Health Points",
+				"value": 130,
+				"color": "#20bd35"
+			},
+			{
+				"label": "Attack Points",
+				"value": 65,
+				"color": "#eb1124"
+			},
+			{
+				"label": "Defense Points",
+				"value": 60,
+				"color": "#383c7e"
+			},
+			{
+				"label": "Special Attack",
+				"value": 110,
+				"color": "#69387e"
+			},
+			{
+				"label": "Special Defense",
+				"value": 95,
+				"color": "#73a417"
+			},
+			{
+				"label": "Speed",
+				"value": 65,
+				"color": "#dcb921"
+			}
+		]
+	},
+	"labels": {
+		"outer": {
+			"pieDistance": 10
+		},
+		"inner": {
+			"format": "value"
+		},
+		"mainLabel": {
+			"font": "verdana",
+			"fontSize": 15
+		},
+		"percentage": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"decimalPlaces": 0
+		},
+		"value": {
+			"color": "#e1e1e1",
+			"font": "verdana",
+			"fontSize": 13
+		},
+		"lines": {
+			"enabled": true,
+			"color": "#cccccc"
+		},
+		"truncation": {
+			"enabled": true
+		}
+	},
+	"effects": {
+		"pullOutSegmentOnClick": {
+			"effect": "linear",
+			"speed": 400,
+			"size": 8
+		}
+	}
+});
